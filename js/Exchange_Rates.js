7720229b995baa1023fb70f774b2d45c1045da51
Anton-Bobr <antonpiatrouski@gmail.com>
2021-04-09 15:04:53 +0300

    +nodejs server

--- js/Exchange_Rates.js
+++ js/Exchange_Rates.js
@@ -1,9 +1,5 @@
 // "use strict"
 
 console.log('Hello World!');
-console.log('Or no');
 
-var requestURL = "https://www.nbrb.by/api/exrates/currencies[/{cur_id}]"
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
 
